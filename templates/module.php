81ebc76500315c949c9daff3483a92cf5d3bb637
Tim Otten <to-git@think.hm>
2012-03-19 20:21:55 -0400

    Copy-paste doc mistake

--- templates/module.php
+++ templates/module.php
@@ -5,7 +5,7 @@ echo "<?php\n";
 require_once '<?= $mainFile ?>.civix.php';
 
 /**
- * (Delegated) Implementation of hook_civicrm_config
+ * Implementation of hook_civicrm_config
  */
 function <?= $mainFile ?>_civicrm_config(&$config) {
   _<?= $mainFile ?>_civix_civicrm_config($config);
