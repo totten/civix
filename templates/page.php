4a28c80d786f9b670ad57e8677a2afa75a5d5863
Tim Otten <to-git@think.hm>
2012-03-24 06:45:28 -0400

    Add comments to the auto-generated page controller+view

--- templates/page.php
+++ templates/page.php
@@ -6,6 +6,7 @@ require_once 'CRM/Core/Page.php';
 
 class <?= preg_replace(':/:','_',$namespace) ?>_Page_<?= $pageClassName ?> extends CRM_Core_Page {
     function run() {
+        // Example: Assign a variable for use in a template
         $this->assign('currentTime', date('Y-m-d H:i:s'));
         parent::run();
     }
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,1 +1,5 @@
-<p>{ts 1=$currentTime}The current time is %1.{/ts}</p>
+{* Example: Display a variable directly *}
+<p>The current time is {$currentTime}</p>
+
+{* Example: Display a translated string -- which happens to include a variable *}
+<p>{ts 1=$currentTime}(In your native language) The current time is %1.{/ts}</p>
