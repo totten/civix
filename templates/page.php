bfc607e61240c56aac7e7dfaf1b7c8a933a6f49a
Tim Otten <to-git@think.hm>
2012-03-24 07:06:32 -0400

    Tweak default title on new pages

--- templates/page.php
+++ templates/page.php
@@ -6,8 +6,12 @@ require_once 'CRM/Core/Page.php';
 
 class <?= preg_replace(':/:','_',$namespace) ?>_Page_<?= $pageClassName ?> extends CRM_Core_Page {
     function run() {
+        // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+        CRM_Utils_System::setTitle(ts('<?= $pageClassName ?>'));
+        
         // Example: Assign a variable for use in a template
         $this->assign('currentTime', date('Y-m-d H:i:s'));
+        
         parent::run();
     }
 }
