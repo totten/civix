eafec45c774c5694fc7a91671a95fe4c3e3347af
Tim Otten <to-git@think.hm>
2012-03-26 14:12:19 -0400

    templates/page.php - Use new whitespace convention

--- templates/page.php
+++ templates/page.php
@@ -5,13 +5,13 @@ echo "<?php\n";
 require_once 'CRM/Core/Page.php';
 
 class <?= preg_replace(':/:','_',$namespace) ?>_Page_<?= $pageClassName ?> extends CRM_Core_Page {
-    function run() {
-        // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-        CRM_Utils_System::setTitle(ts('<?= $pageClassName ?>'));
-        
-        // Example: Assign a variable for use in a template
-        $this->assign('currentTime', date('Y-m-d H:i:s'));
-        
-        parent::run();
-    }
+  function run() {
+    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
+    CRM_Utils_System::setTitle(ts('<?= $pageClassName ?>'));
+    
+    // Example: Assign a variable for use in a template
+    $this->assign('currentTime', date('Y-m-d H:i:s'));
+    
+    parent::run();
+  }
 }
