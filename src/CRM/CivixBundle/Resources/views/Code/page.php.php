a2bc7775ab533dd12c0d9e7b57a92701402ba3b5
Tim Otten <totten@civicrm.org>
2014-01-06 17:35:02 -0800

    generate:form, generate:page - Reduce code duplication

--- src/CRM/CivixBundle/Resources/views/Code/page.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/page.php.php
@@ -4,7 +4,7 @@ echo "<?php\n";
 
 require_once 'CRM/Core/Page.php';
 
-class <?php echo preg_replace(':/:','_',$namespace) ?>_Page_<?php echo $pageClassName ?> extends CRM_Core_Page {
+class <?php echo preg_replace(':/:','_',$fullClassName) ?> extends CRM_Core_Page {
   function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('<?php echo $pageClassName ?>'));
