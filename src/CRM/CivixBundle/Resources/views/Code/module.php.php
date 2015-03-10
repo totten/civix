70a69c8882afe03b437f36e4e6c988d1f1024f59
Eileen McNaughton <eileen@fuzion.co.nz>
2015-03-11 11:33:17 +1300

    comment layout fix

--- src/CRM/CivixBundle/Resources/views/Code/module.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/module.php.php
@@ -66,7 +66,8 @@ function <?php echo $mainFile ?>_civicrm_disable() {
  * @param $op string, the type of operation being performed; 'check' or 'enqueue'
  * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
  *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
  *                for 'enqueue', returns void
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
