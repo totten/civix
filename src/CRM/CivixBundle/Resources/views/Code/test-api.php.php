454d20a59ea17fa62c98d800e25b3e29a1a5956c
Philipp Batroff <batroff@systopia.de>
2017-10-10 00:33:36 +0200

    Added suggestions from @totten fixed template syntax

--- src/CRM/CivixBundle/Resources/views/Code/test-api.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/test-api.php.php
@@ -2,34 +2,50 @@
 echo "<?php\n";
 ?>
 
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
 /**
-* <?php echo $entityNameCamel ?>.<?php echo $actionNameCamel ?> API Test Case
-* This is a generic test class implemented with PHPUnit.
-*/
-class <?php echo $testClassName ?> extends \PHPUnit_Framework_TestCase {
+ * <?php echo $entityNameCamel ?>.<?php echo $actionNameCamel ?> API Test Case
+ * This is a generic test class implemented with PHPUnit.
+ * @group headless
+ */
+class <?php echo $testClassName ?> extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
 
   /**
-  * The setup() method is executed before the test is executed (optional).
-  */
+   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+   * See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+   */
+  public function setUpHeadless() {
+    return \Civi\Test::headless()
+    ->installMe(__DIR__)
+    ->apply();
+  }
+
+  /**
+   * The setup() method is executed before the test is executed (optional).
+   */
   public function setUp() {
     parent::setUp();
   }
 
   /**
-  * The tearDown() method is executed after the test was executed (optional)
-  * This can be used for cleanup.
-  */
+   * The tearDown() method is executed after the test was executed (optional)
+   * This can be used for cleanup.
+   */
   public function tearDown() {
     parent::tearDown();
   }
 
   /**
-  * Simple example test case.
-  *
-  * Note how the function name begins with the word "test".
-  */
-  public function testExample() {
-    self::assertTrue(TRUE, "The argument must be true to pass the test");
+   * Simple example test case.
+   *
+   * Note how the function name begins with the word "test".
+   */
+  public function testApiExample() {
+    $result = civicrm_api3('<?php echo $entityNameCamel ?>', '<?php echo $actionNameCamel ?>', array('magicword' => 'sesame'));
+    $this->assertEquals('Twelve', $result['values'][12]['name']);
   }
 }
 
