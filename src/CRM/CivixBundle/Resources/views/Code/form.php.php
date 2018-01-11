37d1ebe96ec5d43e738ac2d746eeb0a5a57f8463
Tim Otten <totten@civicrm.org>
2018-01-10 17:28:36 -0800

    Resources/views/Code/form.php.php - Use pretty array syntax

--- src/CRM/CivixBundle/Resources/views/Code/form.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/form.php.php
@@ -21,13 +21,13 @@ class <?php echo preg_replace(':/:', '_', $fullClassName) ?> extends CRM_Core_Fo
       $this->getColorOptions(), // list of options
       TRUE // is required
     );
-    $this->addButtons(array(
-      array(
+    $this->addButtons([
+      [
         'type' => 'submit',
         'name' => E::ts('Submit'),
         'isDefault' => TRUE,
-      ),
-    ));
+      ],
+    ]);
 
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
@@ -37,22 +37,22 @@ class <?php echo preg_replace(':/:', '_', $fullClassName) ?> extends CRM_Core_Fo
   public function postProcess() {
     $values = $this->exportValues();
     $options = $this->getColorOptions();
-    CRM_Core_Session::setStatus(E::ts('You picked color "%1"', array(
+    CRM_Core_Session::setStatus(E::ts('You picked color "%1"', [
       1 => $options[$values['favorite_color']],
-    )));
+    ]));
     parent::postProcess();
   }
 
   public function getColorOptions() {
-    $options = array(
+    $options = [
       '' => E::ts('- select -'),
       '#f00' => E::ts('Red'),
       '#0f0' => E::ts('Green'),
       '#00f' => E::ts('Blue'),
       '#f0f' => E::ts('Purple'),
-    );
-    foreach (array('1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e') as $f) {
-      $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', array(1 => $f));
+    ];
+    foreach (['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'] as $f) {
+      $options["#{$f}{$f}{$f}"] = E::ts('Grey (%1)', [1 => $f]);
     }
     return $options;
   }
@@ -67,7 +67,7 @@ class <?php echo preg_replace(':/:', '_', $fullClassName) ?> extends CRM_Core_Fo
     // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
     // items don't have labels.  We'll identify renderable by filtering on
     // the 'label'.
-    $elementNames = array();
+    $elementNames = [];
     foreach ($this->_elements as $element) {
       /** @var HTML_QuickForm_Element $element */
       $label = $element->getLabel();
