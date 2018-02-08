306cd949349bbf12e3be143f92bd1db9a44d7bea
Tim Otten <totten@civicrm.org>
2018-02-08 11:29:14 +0100

    Add skeletal phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+#### Find primary autoloader
+$autoloaders = array(
+  implode(DIRECTORY_SEPARATOR, array(dirname(__DIR__), 'vendor', 'autoload.php')),
+  implode(DIRECTORY_SEPARATOR, array(dirname(dirname(dirname(dirname(__DIR__)))), 'vendor', 'autoload.php')),
+);
+foreach ($autoloaders as $autoloader) {
+  if (file_exists($autoloader)) {
+    $loader = require_once $autoloader;
+    break;
+  }
+}
+if (!isset($loader)) {
+  die("Failed to find autoloader");
+}
+
+#### Extra - Register classes in "tests" directory
+$loader->add('CRM', __DIR__);
