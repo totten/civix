15324a68e889d7557e59e43153f698dacb8b51b2
Elliott Eggleston <ejegg@ejegg.com>
2019-10-08 11:30:33 +0200

    generate:module creates skeletal mkdocs content
    Just generates mkdocs.yml and docs/index.md with a couple of Template
    builders in the main InitCommand for now. Alternatively, could create
    a Docs builder and/or command. Uses the old markdown_extensions syntax
    because that's all the publishing infrastructure supports now.
    
    Fixes #161

---
+++ src/CRM/CivixBundle/Resources/views/Code/mkdocs.yml.php
@@ -0,0 +1,19 @@
+site_name: <?php echo "$fullName\n"; ?>
+repo_url: https://lab.civicrm.org/extensions/FIXME
+theme: material
+
+pages:
+- Home: index.md
+
+markdown_extensions:
+  - attr_list
+  - admonition
+  - def_list
+  - codehilite(guess_lang=false)
+  - toc(permalink=true)
+  - pymdownx.superfences
+  - pymdownx.inlinehilite
+  - pymdownx.tilde
+  - pymdownx.betterem
+  - pymdownx.mark
+  - pymdownx.magiclink
