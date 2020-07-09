9df5b91c5e5c51d758fa11e36617f13573081dc8
Coleman Watts <coleman@civicrm.org>
2020-07-09 10:09:26 -0400

    Use controllerAs for angular best-practice

--- src/CRM/CivixBundle/Resources/views/Code/angular-page.html.php
+++ src/CRM/CivixBundle/Resources/views/Code/angular-page.html.php
@@ -1,7 +1,7 @@
 <div class="crm-container">
-  <div crm-ui-debug="myContact"></div>
+  <div crm-ui-debug="$ctrl.myContact"></div>
 
-  <h1 crm-page-title>{{ts('About %1', {1: myContact.first_name + ' ' + myContact.last_name})}}</h1>
+  <h1 crm-page-title>{{ts('About %1', {1: $ctrl.myContact.first_name + ' ' + $ctrl.myContact.last_name})}}</h1>
 
   <form name="myForm" crm-ui-id-scope>
 
@@ -12,9 +12,9 @@
     </div>
 
     <div crm-ui-accordion="{title: ts('Greeting')}">
-      <p ng-show="myContact.first_name">{{ ts('Hello, %1.', {1: myContact.first_name}) }}</p>
+      <p ng-show="$ctrl.myContact.first_name">{{ ts('Hello, %1.', {1: $ctrl.myContact.first_name}) }}</p>
 
-      <p ng-show="!myContact.first_name">{{ ts('Hello, stranger.') }}</p>
+      <p ng-show="!$ctrl.myContact.first_name">{{ ts('Hello, stranger.') }}</p>
     </div>
 
     <div crm-ui-accordion="{title: ts('About Me')}">
@@ -24,14 +24,14 @@
             <input
               crm-ui-id="myForm.first_name"
               name="first_name"
-              ng-model="myContact.first_name"
+              ng-model="$ctrl.myContact.first_name"
               class="crm-form-text"
               placeholder="{{ts('First')}}"
               />
             <input
               crm-ui-id="myForm.last_name"
               name="last_name"
-              ng-model="myContact.last_name"
+              ng-model="$ctrl.myContact.last_name"
               class="crm-form-text"
               placeholder="{{ts('Last')}}"
               />
@@ -40,7 +40,7 @@
       </div>
 
       <div>
-        <button ng-click="save()">{{ts('Save')}}</button>
+        <button ng-click="$ctrl.save()">{{ts('Save')}}</button>
       </div>
     </div>
 
