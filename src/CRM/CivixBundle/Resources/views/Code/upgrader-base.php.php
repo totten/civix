29efa7fd8621f5fd17ab650af84de1981ee36fc1
Tim Otten <totten@civicrm.org>
2020-11-13 19:35:42 -0800

    (REF) Extract UpgraderTasksTrait. Use ClassBuilder.

--- src/CRM/CivixBundle/Resources/views/Code/upgrader-base.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/upgrader-base.php.php
@@ -1,29 +1,13 @@
 <?php
-echo "<?php\n";
+echo "<?php\n\n";
 $_namespace = preg_replace(':/:', '_', $namespace);
-?>
 
-// AUTO-GENERATED FILE -- Civix may overwrite any changes made to this file
-use <?php echo $_namespace ?>_ExtensionUtil as E;
-
-/**
- * Base class which provides helpers to execute upgrade logic
- */
-<?php
-
-function copyClass($inClass, $outClass) {
-  $c = new \ReflectionClass($inClass);
-  $code = file_get_contents($c->getFileName());
-  $lines = explode("\n", $code);
-  $code = implode("\n", array_slice($lines,
-    $c->getStartLine() - 1,
-    $c->getEndLine() - $c->getStartLine() + 1)
-    ) . "\n";
-  $code = str_replace($inClass, $outClass, $code);
-  return $code;
-}
-
-echo copyClass(
-  'CRM_CivixBundle_Resources_Example_UpgraderBase',
-  $_namespace . '_Upgrader_Base'
-);
+echo \CRM\CivixBundle\Utils\ClassBuilder::create($_namespace . '_Upgrader_Base')
+  ->setInClass('CRM_CivixBundle_Resources_Example_UpgraderBase')
+  ->addComments([
+    'Base class which provides helpers to execute upgrade logic',
+    '',
+    'AUTO-GENERATED FILE -- Civix may overwrite any changes made to this file',
+  ])
+  ->addUses(sprintf('use %s_ExtensionUtil as E;', $_namespace))
+  ->toPHP();
