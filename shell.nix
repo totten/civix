9dacb0c8abc80e95507dcf6b0b9666f72857a062
Tim Otten <totten@civicrm.org>
2022-10-10 00:13:28 +0100

    shell.nix - Pin php version

--- shell.nix
+++ shell.nix
@@ -3,7 +3,16 @@
  *
  * Ex: `nix-shell --run ./build.sh`
  */
-{ pkgs ? import <nixpkgs> {} }:
+# { pkgs ? import <nixpkgs> {} }:
+let
+  pkgSrc = fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
+    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+  };
+  pkgs = import pkgSrc {};
+
+in
+
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [ pkgs.php74 pkgs.php74Packages.composer ];
