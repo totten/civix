6c05e6345a79a63f05bb2fbd7d666fe4ad70dc54
Tim Otten <totten@civicrm.org>
2022-11-22 15:32:07 -0800

    upgrades/22.12.0 - Add explanatory note

--- upgrades/22.12.0.up.php
+++ upgrades/22.12.0.up.php
@@ -8,6 +8,15 @@ return function (\CRM\CivixBundle\Upgrader $upgrader) {
   $prefix = $upgrader->infoXml->getFile();
 
   if (is_dir($upgrader->baseDir->string('xml/schema/CRM'))) {
+    $upgrader->io->note([
+      'Civix 22.12 removes `*_civix_civicrm_entityTypes` in favor of a mixin `entity-types-php@1.0`.',
+      'This reduces code-duplication and will enable easier updates in the future.',
+      'This may raise the minimum requirements to CiviCRM v5.45.',
+    ]);
+    if (!$upgrader->io->confirm('Continue with upgrade?')) {
+      throw new \RuntimeException('User stopped upgrade');
+    }
+
     $upgrader->addMixins(['entity-types-php@1.0']);
   }
 
