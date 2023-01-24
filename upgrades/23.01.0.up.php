2e6cba1925d9de7207968b05c54b6729529d9ce5
Tim Otten <totten@civicrm.org>
2023-01-24 13:26:56 -0800

    23.01.0.up.php - Show better explanation/preview

--- upgrades/23.01.0.up.php
+++ upgrades/23.01.0.up.php
@@ -1,40 +1,45 @@
 <?php
+use CRM\CivixBundle\Utils\Formatting;
 
 return function (\CRM\CivixBundle\Upgrader $upgrader) {
   /* @var \Symfony\Component\Console\Style\SymfonyStyle $io */
   $io = $upgrader->io;
-  $prefix = $upgrader->infoXml->getFile();
 
-  $io->note([
-    "Civix v23.01 updates the Smarty registration:",
-    "- Previously, Smarty boilerplate was added to \"hook_config\" unconditionally.\n" .
-    "- Now, the mixin \"smarty-v2\" will be enabled if needed.",
-    "This change reduces boilerplate and improves maintainability.",
-  ]);
-
-  $io->warning([
-    "If this extension uses Smarty with a lifecycle hook (install,enable,disable,uninstall,upgrade), then please re-test them.",
-  ]);
-
-  if (!$io->confirm('Continue with upgrade?')) {
-    throw new \RuntimeException('User stopped upgrade');
-  }
+  $previewChanges = [];
+  $previewChanges[] = ['*_civix_civicrm_config', 'Remove Smarty boilerplate'];
 
   // Do we need a mixin like `ang-php`? Maybe... check whether we have files like `*.ang.php`.
   $filePatterns = [
     'find:*.tpl' => 'smarty-v2@1.0.0',
   ];
   $mixins = array_filter($filePatterns,
-    function (string $mixin, string $pattern) use ($upgrader, $io) {
+    function (string $mixin, string $pattern) use ($upgrader, $io, &$previewChanges) {
       $flagFiles = $upgrader->baseDir->search($pattern);
-      $io->note($flagFiles
-        ? "Enable \"$mixin\". There are files matching pattern \"$pattern\"."
-        : "Skip \"$mixin\". There are no files matching pattern \"$pattern\"."
-      );
+      $previewChanges[] = [
+        'info.xml',
+        $flagFiles ? "Enable $mixin" : "Skip $mixin. (No files match \"$pattern\")",
+      ];
       return (bool) $flagFiles;
     },
     ARRAY_FILTER_USE_BOTH
   );
+
+  $io->note([
+    "Civix v23.01 simplifies the boilerplate used for Smarty registration.",
+    "The following may be affected:",
+    Formatting::ol("%-31s %s\n", $previewChanges),
+  ]);
+
+  if (!empty($mixins)) {
+    $io->warning([
+      "If Smarty templates are used by any lifecycle hooks (install,enable,disable,uninstall,upgrade,managed), then please re-test them.",
+    ]);
+  }
+
+  if (!$io->confirm('Continue with upgrade?')) {
+    throw new \RuntimeException('User stopped upgrade');
+  }
+
   $upgrader->addMixins($mixins);
 
 };
