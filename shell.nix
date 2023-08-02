e08141365942ec6f54084a1734294fa38b0ef5bb
Tim Otten <totten@civicrm.org>
2023-08-02 01:35:36 -0700

    shell.nix - Import key definitions from buildkit

--- shell.nix
+++ shell.nix
@@ -1,24 +1,42 @@
 /**
  * This shell is suitable for compiling civix.phar.... and not much else.
  *
- * Ex: `nix-shell --run ./build.sh`
+ * Ex: `nix-shell --run ./scripts/build.sh`
  */
-# { pkgs ? import <nixpkgs> {} }:
+
+{ pkgs ? import <nixpkgs> {} }:
+
 let
-  pkgSrc = fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/ce6aa13369b667ac2542593170993504932eb836.tar.gz";
-    sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
-  };
-  pkgs = import pkgSrc {};
-  myphp = pkgs.php81.buildEnv {
-    extraConfig = ''
-      memory_limit=-1
-    '';
-  };
+
+  buildkit = import (pkgs.fetchFromGitHub {
+    owner = "totten";
+    repo = "civicrm-buildkit";
+    rev = "153371e9bdcb22392b878cca545df0888fb61925";
+    sha256 = "sha256-rdwmA4uqIqfqXu2f+ewVH0Gs/BzcB13p8oRbbTdUsAs=";
+  });
+
+  ## If you're trying to patch buildkit at the sametime, then use a local copy:
+  #buildkit = import ((builtins.getEnv "HOME") + "/bknix/default.nix");
 
 in
 
   pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ myphp pkgs.php81Packages.composer ];
-}
+    nativeBuildInputs = buildkit.profiles.base ++ [
+
+      (buildkit.pins.v2305.php81.buildEnv {
+        extraConfig = ''
+          memory_limit=-1
+        '';
+      })
+
+      buildkit.pkgs.box
+      buildkit.pkgs.composer
+      buildkit.pkgs.pogo
+      buildkit.pkgs.phpunit8
+
+      pkgs.bash-completion
+    ];
+    shellHook = ''
+      source ${pkgs.bash-completion}/etc/profile.d/bash_completion.sh
+    '';
+  }
