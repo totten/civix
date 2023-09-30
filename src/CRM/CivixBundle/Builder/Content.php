ba584a18c2fff42443f4ce5df838a093fc0040d0
colemanw <coleman@civicrm.org>
2023-09-30 10:26:30 -0400

    Add Builder/Content

---
+++ src/CRM/CivixBundle/Builder/Content.php
@@ -0,0 +1,61 @@
+<?php
+namespace CRM\CivixBundle\Builder;
+
+use CRM\CivixBundle\Builder;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Simply write a string to a file
+ */
+class Content implements Builder {
+
+  private $content;
+  protected $path;
+  protected $overwrite;
+
+  /**
+   * @param string $content
+   * @param string $path
+   * @param bool|string $overwrite TRUE (always overwrite), FALSE (preserve with error), 'ignore' (preserve quietly)
+   * @param
+   */
+  public function __construct(string $content, string $path, $overwrite = FALSE) {
+    $this->content = $content;
+    $this->path = $path;
+    $this->overwrite = $overwrite;
+  }
+
+  public function loadInit(&$ctx) {
+  }
+
+  public function init(&$ctx) {
+  }
+
+  public function load(&$ctx) {
+  }
+
+  /**
+   * Write the content
+   */
+  public function save(&$ctx, OutputInterface $output) {
+    $parent = dirname($this->path);
+    if (!is_dir($parent)) {
+      mkdir($parent, Dirs::MODE, TRUE);
+    }
+    if (file_exists($this->path) && $this->overwrite === 'ignore') {
+      // do nothing
+    }
+    elseif (file_exists($this->path) && !$this->overwrite) {
+      $output->writeln("<error>Skip " . $this->path . ": file already exists</error>");
+    }
+    else {
+      $output->writeln("<info>Write</info> " . $this->path);
+      file_put_contents($this->path, $this->getContent($ctx));
+    }
+  }
+
+  protected function getContent($ctx): string {
+    return $this->content;
+  }
+
+}
--- src/CRM/CivixBundle/Builder/Template.php
+++ src/CRM/CivixBundle/Builder/Template.php
@@ -1,20 +1,16 @@
 <?php
 namespace CRM\CivixBundle\Builder;
 
-use CRM\CivixBundle\Builder;
 use CRM\CivixBundle\Services;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Build/update a file based on a template
  */
-class Template implements Builder {
+class Template extends Content {
 
   protected $template;
-  protected $path;
   protected $xml;
   protected $templateEngine;
-  protected $enable;
 
   /**
    * @param string $template
@@ -23,41 +19,15 @@ class Template implements Builder {
    * @param \Symfony\Component\Templating\EngineInterface|null $templateEngine
    * @param
    */
-  public function __construct(string $template, string $path, $overwrite, $templateEngine = NULL) {
+  public function __construct(string $template, string $path, $overwrite = FALSE, $templateEngine = NULL) {
     $this->template = $template;
     $this->path = $path;
     $this->overwrite = $overwrite;
     $this->templateEngine = $templateEngine ?: Services::templating();
-    $this->enable = FALSE;
   }
 
-  public function loadInit(&$ctx) {
-  }
-
-  public function init(&$ctx) {
-  }
-
-  public function load(&$ctx) {
-  }
-
-  /**
-   * Write the xml document
-   */
-  public function save(&$ctx, OutputInterface $output) {
-    $parent = dirname($this->path);
-    if (!is_dir($parent)) {
-      mkdir($parent, Dirs::MODE, TRUE);
-    }
-    if (file_exists($this->path) && $this->overwrite === 'ignore') {
-      // do nothing
-    }
-    elseif (file_exists($this->path) && !$this->overwrite) {
-      $output->writeln("<error>Skip " . $this->path . ": file already exists</error>");
-    }
-    else {
-      $output->writeln("<info>Write</info> " . $this->path);
-      file_put_contents($this->path, $this->templateEngine->render($this->template, $ctx));
-    }
+  protected function getContent($ctx): string {
+    return $this->templateEngine->render($this->template, $ctx);
   }
 
 }
