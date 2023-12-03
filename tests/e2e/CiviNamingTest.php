6386471b58bf9e482baf515be58bddac8632b4e7
Tim Otten <totten@civicrm.org>
2023-12-03 13:16:25 +0000

    Add tests for helper createClassVars()

--- tests/e2e/CiviNamingTest.php
+++ tests/e2e/CiviNamingTest.php
@@ -8,11 +8,11 @@ use CRM\CivixBundle\Utils\Path;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\NullOutput;
 
-class CRMNamingTest extends \PHPUnit\Framework\TestCase {
+class CiviNamingTest extends \PHPUnit\Framework\TestCase {
 
   use CivixProjectTestTrait;
 
-  public static $key = 'civix_crmnaming';
+  public static $key = 'civix_civinaming';
 
   /**
    * @var \CRM\CivixBundle\Upgrader
@@ -27,41 +27,40 @@ class CRMNamingTest extends \PHPUnit\Framework\TestCase {
 
     $this->assertFileGlobs([
       'info.xml' => 1,
-      'civix_crmnaming.php' => 1,
-      'civix_crmnaming.civix.php' => 1,
+      'civix_civinaming.php' => 1,
+      'civix_civinaming.civix.php' => 1,
     ]);
 
     $this->upgrader = new Upgrader(new ArgvInput(), new NullOutput(), new Path(static::getExtPath()));
     $this->upgrader->updateInfo(function(Info $info) {
-      // FIXME: Allow "_" instead of "/"
-      $info->get()->civix->namespace = 'CRM/NamingTest';
+      // FIXME: Allow "\" instead of "/"
+      $info->get()->civix->namespace = 'Civi/NamingTest';
     });
-
   }
 
   public function testNaming_OnePart(): void {
     $vars = $this->upgrader->createClassVars('Widget');
     $this->assertTrue(is_string($vars['extBaseDir']) && is_dir($vars['extBaseDir']));
-    $this->assertEquals('civix_crmnaming', $vars['extMainFile']);
-    $this->assertEquals('civix_crmnaming', $vars['extKey']);
-    $this->assertEquals('CRM/NamingTest/Widget.php', $vars['classFile']);
-    $this->assertEquals('CRM_NamingTest_Widget', $vars['className']);
-    $this->assertEquals('CRM_NamingTest_Widget', $vars['classNameFull']);
-    $this->assertEquals('', $vars['classNamespace']);
-    $this->assertEquals('', $vars['classNamespaceDecl']);
+    $this->assertEquals('civix_civinaming', $vars['extMainFile']);
+    $this->assertEquals('civix_civinaming', $vars['extKey']);
+    $this->assertEquals('Civi/NamingTest/Widget.php', $vars['classFile']);
+    $this->assertEquals('Widget', $vars['className']);
+    $this->assertEquals('Civi\\NamingTest\\Widget', $vars['classNameFull']);
+    $this->assertEquals('Civi\\NamingTest', $vars['classNamespace']);
+    $this->assertEquals('namespace Civi\\NamingTest;', $vars['classNamespaceDecl']);
     $this->assertEquals('use CRM_NamingTest_ExtensionUtil as E;', $vars['useE']);
   }
 
   public function testNaming_TwoParts(): void {
     $vars = $this->upgrader->createClassVars(['Widget', 'Gizmo']);
     $this->assertTrue(is_string($vars['extBaseDir']) && is_dir($vars['extBaseDir']));
-    $this->assertEquals('civix_crmnaming', $vars['extMainFile']);
-    $this->assertEquals('civix_crmnaming', $vars['extKey']);
-    $this->assertEquals('CRM/NamingTest/Widget/Gizmo.php', $vars['classFile']);
-    $this->assertEquals('CRM_NamingTest_Widget_Gizmo', $vars['className']);
-    $this->assertEquals('CRM_NamingTest_Widget_Gizmo', $vars['classNameFull']);
-    $this->assertEquals('', $vars['classNamespace']);
-    $this->assertEquals('', $vars['classNamespaceDecl']);
+    $this->assertEquals('civix_civinaming', $vars['extMainFile']);
+    $this->assertEquals('civix_civinaming', $vars['extKey']);
+    $this->assertEquals('Civi/NamingTest/Widget/Gizmo.php', $vars['classFile']);
+    $this->assertEquals('Gizmo', $vars['className']);
+    $this->assertEquals('Civi\\NamingTest\\Widget\\Gizmo', $vars['classNameFull']);
+    $this->assertEquals('Civi\\NamingTest\\Widget', $vars['classNamespace']);
+    $this->assertEquals('namespace Civi\\NamingTest\\Widget;', $vars['classNamespaceDecl']);
     $this->assertEquals('use CRM_NamingTest_ExtensionUtil as E;', $vars['useE']);
   }
 
