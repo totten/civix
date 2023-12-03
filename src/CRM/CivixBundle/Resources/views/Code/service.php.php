fb50a6b6e77e1fa148216bc0a0ffa146b489f4e0
Tim Otten <totten@civicrm.org>
2023-12-03 18:42:00 +0000

    service.php.php - More comments

--- src/CRM/CivixBundle/Resources/views/Code/service.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/service.php.php
@@ -13,6 +13,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class <?php echo $className; ?> extends AutoService implements EventSubscriberInterface {
 
+  // TIP: Many services implement `EventSubscriberInterface`. However, this can be omitted if you don't need it.
+
   public static function getSubscribedEvents(): array {
     return [
       // '&hook_civicrm_alterContent' => ['onAlterContent', 0],
@@ -22,12 +24,12 @@ class <?php echo $className; ?> extends AutoService implements EventSubscriberIn
   }
 
   // /**
-  //  * @see \CRM_Utils_Hook::alterContent
+  //  * @see \CRM_Utils_Hook::alterContent()
   //  */
   // public function onAlterContent(&$content, $context, $tplName, &$object) { ... }
 
   // /**
-  //  * @see \CRM_Utils_Hook::postCommit
+  //  * @see \CRM_Utils_Hook::postCommit()
   //  */
   // public function onContribute($op, $objectName, $objectId, $objectRef = NULL) { ... }
 
