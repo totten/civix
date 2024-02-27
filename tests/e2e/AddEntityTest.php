ab0e4b6da84d9ec013d130edca437f95d886d2c0
Tim Otten <totten@civicrm.org>
2024-02-27 02:06:02 -0500

    AddEntityTest - Update for dynamic SQL

--- tests/e2e/AddEntityTest.php
+++ tests/e2e/AddEntityTest.php
@@ -11,24 +11,23 @@ class AddEntityTest extends \PHPUnit\Framework\TestCase {
   private $entityFiles = [
     'CRM/CivixAddentity/DAO/Bread.php',
     'CRM/CivixAddentity/BAO/Bread.php',
-    'xml/schema/CRM/CivixAddentity/Bread.xml',
-    'xml/schema/CRM/CivixAddentity/Bread.entityType.php',
+    'schema/Bread.entityType.php',
 
     'CRM/CivixAddentity/DAO/Sandwich.php',
     'CRM/CivixAddentity/BAO/Sandwich.php',
-    'xml/schema/CRM/CivixAddentity/Sandwich.xml',
-    'xml/schema/CRM/CivixAddentity/Sandwich.entityType.php',
+    'schema/Sandwich.entityType.php',
 
     'CRM/CivixAddentity/DAO/Flour.php',
     'CRM/CivixAddentity/BAO/Flour.php',
-    'xml/schema/CRM/CivixAddentity/Flour.xml',
-    'xml/schema/CRM/CivixAddentity/Flour.entityType.php',
+    'schema/Flour.entityType.php',
   ];
 
   public function setUp(): void {
     chdir(static::getWorkspacePath());
     static::cleanDir(static::getKey());
-    $this->civixGenerateModule(static::getKey());
+    $this->civixGenerateModule(static::getKey(), [
+      '--compatibility' => '5.69',
+    ]);
     chdir(static::getKey());
 
     $this->assertFileGlobs([
@@ -44,39 +43,32 @@ class AddEntityTest extends \PHPUnit\Framework\TestCase {
     ]);
     $this->assertFileGlobs(array_fill_keys($this->entityFiles, 0));
 
+    $this->civixGenerateUpgrader();
     $this->civixGenerateEntity('Bread');
     $this->civixGenerateEntity('Sandwich');
     $this->civixGenerateEntity('Meal');
     $this->civixGenerateEntity('Flour');
-    $this->civixGenerateEntityBoilerplate();
     $this->addExampleFK($this->getExtPath('xml/schema/CRM/CivixAddentity/Bread.xml'), 'flour', 'civicrm_flour');
     $this->addExampleFK($this->getExtPath('xml/schema/CRM/CivixAddentity/Sandwich.xml'), 'bread', 'civicrm_bread');
     $this->addExampleFK($this->getExtPath('xml/schema/CRM/CivixAddentity/Meal.xml'), 'sandwich', 'civicrm_sandwich');
     // add FK referencing its own table
     $this->addExampleFK($this->getExtPath('xml/schema/CRM/CivixAddentity/Meal.xml'), 'next_meal', 'civicrm_meal');
-    $this->civixGenerateEntityBoilerplate();
 
     $this->assertFileGlobs([
-      'sql/auto_install.sql' => 1,
+      // No longer use static files
+      'sql/auto_install.sql' => 0,
     ]);
     $this->assertFileGlobs(array_fill_keys($this->entityFiles, 1));
 
-    $install = $this->grepLines(';CREATE TABLE;', $this->getExtPath('sql/auto_install.sql'));
-    $uninstall = $this->grepLines(';DROP TABLE;', $this->getExtPath('sql/auto_uninstall.sql'));
-
-    $this->assertEquals([
-      'CREATE TABLE `civicrm_flour` (',
-      'CREATE TABLE `civicrm_bread` (',
-      'CREATE TABLE `civicrm_sandwich` (',
-      'CREATE TABLE `civicrm_meal` (',
-    ], $install);
-
-    $this->assertEquals([
-      'DROP TABLE IF EXISTS `civicrm_meal`;',
-      'DROP TABLE IF EXISTS `civicrm_sandwich`;',
-      'DROP TABLE IF EXISTS `civicrm_bread`;',
-      'DROP TABLE IF EXISTS `civicrm_flour`;',
-    ], $uninstall);
+    // FIXME: Perhaps call `cv ev 'E::schema()->generateInstallSql()` and restore the old assertions
+    $this->assertEquals('CiviMix\\Schema\\CivixAddentity\\AutomaticUpgrader', trim($this->civixInfoGet('upgrader')->getDisplay()));
+    $civixPhpFile = $this->getExtPath('civix_addentity.civix.php');
+    $content = file_get_contents($civixPhpFile);
+    $this->assertStringSequence([
+      '($GLOBALS[\'_PathLoad\'][0] ?? require __DIR__ . \'/mixin/lib/pathload-0.php\');',
+      'pathload()->addSearchDir(__DIR__ . \'/mixin/lib\');',
+      '    pathload()->loadPackage(\'civimix-schema@5\', TRUE);',
+    ], $content);
   }
 
   private function grepLines(string $pattern, string $file): array {
