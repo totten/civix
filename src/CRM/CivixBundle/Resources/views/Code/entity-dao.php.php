96e6e4879742f3d3d71f56c7c741e5eb7d9df0f5
Tim Otten <totten@civicrm.org>
2024-06-27 00:42:48 -0700

    Replace `CRM_Core_DAO_Base` with CiviMix\Schema\*\DAO
    In this arrangement, the anonymous DAO implementation is used on any
    environment.
    
    This means that it will choose the highest available version of the DAO-base
    at the moment of loading, so it presumptively supports future backporting.
    
    However, there may be some micro-optimization questions in this implementation.
    
    This variant has... OK support for IDE hints/PHPStorm. The drill-down will
    technically point to the wrong file (eg taking you to
    `CRM/Core/DAO/Base.php` instead of `mixin/lib/civimix-schema/src/DAO.php`).
    However, the autocomplete suggestions should be on-point.

--- src/CRM/CivixBundle/Resources/views/Code/entity-dao.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/entity-dao.php.php
@@ -1,8 +1,15 @@
 <?php
 echo "<?php\n";
 $_namespace = preg_replace(':/:', '_', $namespace);
+$_clPrefix = 'CiviMix\\Schema\\' . \CRM\CivixBundle\Utils\Naming::createCamelName($mainFile) . '\\'
+
 ?>
 use <?php echo $_namespace ?>_ExtensionUtil as E;
+
+if (FALSE) {
+  // Generate IDE hints that are fairly close to reality.
+  class_alias('CRM_Core_DAO_Base', <?php var_export($_clPrefix . "DAO"); ?>);
+}
 
 /**
  * DAOs provide an OOP-style facade for reading and writing database records.
@@ -13,6 +20,9 @@ use <?php echo $_namespace ?>_ExtensionUtil as E;
  * This stub provides compatibility. It is not intended to be modified in a
  * substantive way. However, you may add comments and annotations.
  */
-class <?php echo $daoClassName ?> extends CRM_Core_DAO_Base {
+class <?php echo $daoClassName ?> extends <?php echo $_clPrefix . "DAO"; ?> {
+
+  // Required by some versions of CiviCRM.
+  public static $_tableName = <?php var_export($tableName); ?>;
 
 }
