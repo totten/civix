2aabdb321b01215d5ad18335b12774f8b5f2a0bd
Tim Otten <totten@civicrm.org>
2024-06-27 00:42:48 -0700

    Replace `CRM_Core_DAO_Base` with (mostly) static alias `CRM_*_DAO_Base`
    * This approach defines `CRM_*_DAO_Base`.
    
    * When deployed on 5.74+, `CRM_*_DAO_Base` will be exactly the same as `CRM_Core_DAO_Base`
      In other words:
    
        class CRM_Foo_DAO_MyData extends CRM_Foo_DAO_Base {}
        class_alias('CRM_Core_DAO_Base', 'CRM_Foo_DAO_Base');
        class CRM_Core_DAO_Base extends CRM_Core_DAO {}
    
    * When deployed on <5.74, `CRM_*_DAO_Base` will use the anonymous backport
      variant from civimix. In other words:
    
        class CRM_Foo_DAO_MyData extends CRM_Foo_DAO_Base {}
        class_alias('CiviMix\Schema\Foo\DAO', 'CRM_Foo_DAO_Base');
        ^^ Read anonymous class... which extends CRM_Core_DAO
    
    * In this arrangement, the anonymous DAO implementation is -only- used on
      old versions. Once you get to 5.74+, you use the local base. This should
      provide better IDE hints and simpler callstacks. You presumptively do not
      get backports of changes/improvements in the DAO base.

--- src/CRM/CivixBundle/Resources/views/Code/entity-dao.php.php
+++ src/CRM/CivixBundle/Resources/views/Code/entity-dao.php.php
@@ -13,6 +13,9 @@ use <?php echo $_namespace ?>_ExtensionUtil as E;
  * This stub provides compatibility. It is not intended to be modified in a
  * substantive way. However, you may add comments and annotations.
  */
-class <?php echo $daoClassName ?> extends CRM_Core_DAO_Base {
+class <?php echo $daoClassName ?> extends <?php echo $_namespace . '_DAO_Base'; ?> {
+
+  // Required by some versions of CiviCRM.
+  public static $_tableName = <?php var_export($tableName); ?>;
 
 }
