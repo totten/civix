c15198c24a2899b24d67987d20eb10a58feea1c1
Tim Otten <totten@civicrm.org>
2025-01-28 21:57:20 -0800

    Replace old Command::confirm() with Civix::io()->confirm()
    * Before Civix::io() was available, the `AbstractCommand` had a helper called `confirm().
    * However, this helper could only be used directly within a `Command` class.
    * Now, with Civix::io(), it's reuse helpers in many contexts (commands, generators, upgraders, etc)

--- src/CRM/CivixBundle/Command/AbstractCommand.php
+++ src/CRM/CivixBundle/Command/AbstractCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 abstract class AbstractCommand extends Command {
 
@@ -37,19 +36,6 @@ abstract class AbstractCommand extends Command {
     finally {
       \Civix::ioStack()->pop();
     }
-  }
-
-  protected function confirm(InputInterface $input, OutputInterface $output, $message, $default = TRUE) {
-    $message = '<info>' . $message . '</info>'; /* FIXME Let caller stylize */
-    if ($input->getOption('yes')) {
-      $output->writeln($message . ($default ? 'Y' : 'N'));
-      return $default;
-    }
-
-    /** @var \Symfony\Component\Console\Helper\QuestionHelper $helper */
-    $helper = $this->getHelper('question');
-    $question = new ConfirmationQuestion($message, $default);
-    return (bool) $helper->ask($input, $output, $question);
   }
 
   protected function getModuleInfo(&$ctx): Info {
